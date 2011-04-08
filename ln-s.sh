66fb022a37428cabcecdaf391d8ee7304a203c53
森井ゴンザレス <morygonzalez@gmail.com>
2011-04-08 10:27:43 +0900

    added shellscript

---
+++ ln-s.sh
@@ -0,0 +1,6 @@
+ln -fs $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -fs $HOME/dotfiles/.zsh $HOME/.zsh
+ln -fs $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -fs $HOME/dotfiles/.vim $HOME/.vim
+ln -fs $HOME/dotfiles/.screenrc $HOME/.screenrc
+ln -fs $HOME/dotfiles/.bashrc $HOME/.bashrc
