2e2e0334e6a54d18e64316ea1cb712bdf5f10747
森井ゴンザレス <morygonzalez@gmail.com>
2013-09-04 14:35:25 +0900

    move .emacs to .emacs.d/init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,6 +8,7 @@
 
 ;; バックアップフィアル作らない
 (setq make-backup-files nil)
+(setq auto-save-default nil)
 
 ;; カーソル位置表示
 (column-number-mode t)
@@ -15,17 +16,28 @@
 (global-linum-mode t)
 (setq linum-format "%3d ")
 
+;; package
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
 (custom-set-variables
  ;; Custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("1ac9a474d289e6a44894d1b484b3aa5eb345cea6ed6f32ec5214c797ac7ddf23" "9fd20670758db15cc4d0b4442a74543888d2e445646b25f2755c65dcd6f1504b" default))))
+ )
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+
+ ;; font
+ '(default ((t (:height: 120 :background: "black" :foreground: "white" :weight normal :width: normal :slant normal :family "Menlo"))))
+ '(mode-line ((t (:foreground "skyblue" :background "#222222" :box (:line-width 1 :color nil :style released-button)))))
  )
 
-(load-theme 'monokai t)
+(load-theme 'molokai t)
