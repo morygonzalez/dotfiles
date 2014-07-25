e3212ec6d55054c2d7b2c02db873fcf98230c610
森井ゴンザレス <morygonzalez@gmail.com>
2014-07-26 07:27:21 +0900

    zsh: miyagawa さんの peco & ghq のやつを剽窃
    ghq + peco/percol - Tatsuhiko Miyagawa's blog
    http://weblog.bulknews.net/post/89635306479/ghq-peco-percol

--- .zsh/functions/peco.zsh
+++ .zsh/functions/peco.zsh
@@ -33,3 +33,14 @@ function peco-git-recent-branches () {
 }
 zle -N peco-git-recent-branches
 bindkey '^b' peco-git-recent-branches
+
+function peco-src () {
+    local selected_dir=$(ghq list --full-path | peco --query "$LBUFFER")
+    if [ -n "$selected_dir" ]; then
+        BUFFER="cd ${selected_dir}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N peco-src
+bindkey '^]' peco-src
