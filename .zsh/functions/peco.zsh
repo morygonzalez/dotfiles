1b7cb1988c5ddf5a96ed33dc8f076063dd9e60be
森井ゴンザレス <morygonzalez@gmail.com>
2015-07-15 10:28:32 +0900

    zsh: ec2ls with peco

--- .zsh/functions/peco.zsh
+++ .zsh/functions/peco.zsh
@@ -46,3 +46,14 @@ function peco-bundle-open() {
 }
 zle -N peco-bundle-open
 bindkey '^o' peco-bundle-open
+
+function peco-ec2ls () {
+    local selected_host=$(ec2ls | expand | peco --query "$LBUFFER" | awk '{print $4}')
+    if [ -n "$selected_host" ]; then
+        BUFFER="ssh ${selected_host}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N peco-ec2ls
+bindkey '^xs' peco-ec2ls
