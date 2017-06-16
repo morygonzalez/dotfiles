4f7595542cbdc59684c34a52fd5ccf1639db83eb
Hitoshi Nakashima <hitoshi.nakashima@gmail.com>
2017-06-16 11:12:30 +0900

    peco: Use OSS ec2ls

--- .config/fish/functions/peco_ec2ls.fish
+++ .config/fish/functions/peco_ec2ls.fish
@@ -1,5 +1,6 @@
 function peco_ec2ls
-  ec2ls | expand | peco --query "$argv" | awk '{print $6}' | read selected_host
+  ec2ls | peco --query "$argv" | awk -F\t '{print $5}' | read selected_host
+  # ec2ls | expand | peco --query "$argv" | awk '{print $5}' | read selected_host
   if [ $selected_host ]
     commandline "ssh $selected_host"
   else
