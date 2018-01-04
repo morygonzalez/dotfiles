07d3fea92943ff6597fcba47ccaec4fc8bf9c7f6
Hitoshi Nakashima <hitoshi.nakashima@gmail.com>
2018-01-04 14:41:31 +0900

    fish: Update peco_ec2ls

--- .config/fish/functions/peco_ec2ls_production.fish
+++ .config/fish/functions/peco_ec2ls_production.fish
@@ -1,7 +1,12 @@
 function peco_ec2ls_production
-  ec2ls | grep -v 'dev' | expand | peco --query "$argv" | awk '{print $7}' | tr -d '[:space:]' | read selected_host
+  ec2ls | grep -v 'dev' | peco --query "$argv" | awk -F\t '{print $6 $3}' | read selected_host name
+  echo $selected_host | tr -d '[:space:]' | read selected_host
   if [ $selected_host ]
-    commandline "ssh $selected_host"
+    if string match -r '(ECS|redash)' $name
+      commandline "ssh ec2-user@$selected_host -i ~/.ssh/keys/yamap.pem"
+    else
+      commandline "ssh $selected_host"
+    end
   else
     commandline ''
   end
