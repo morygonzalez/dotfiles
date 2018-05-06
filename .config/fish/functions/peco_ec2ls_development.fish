6b70a2f442635deae8782f2e15b0523286d836cf
Hitoshi Nakashima <hitoshi.nakashima@gmail.com>
2018-05-06 17:49:22 +0900

    fish: peco shortcut update

--- .config/fish/functions/peco_ec2ls_development.fish
+++ .config/fish/functions/peco_ec2ls_development.fish
@@ -2,7 +2,7 @@ function peco_ec2ls_development
   ec2ls | grep 'dev' | peco --query "$argv" | awk -F\t '{print $4 $3}' | read selected_host name
   echo $selected_host | tr -d '[:space:]' | read selected_host
   if [ $selected_host ]
-    if string match -r '(ECS|gyazo)' $name
+    if string match -ir '(ECS|gyazo)' $name
       commandline "ssh ec2-user@$selected_host -i ~/.ssh/keys/yamap.pem"
     else
       commandline "ssh webapp@$selected_host"
--- .config/fish/functions/peco_ec2ls_production.fish
+++ .config/fish/functions/peco_ec2ls_production.fish
@@ -2,8 +2,10 @@ function peco_ec2ls_production
   ec2ls | grep -v 'dev' | peco --query "$argv" | awk -F\t '{print $6 $3}' | read selected_host name
   echo $selected_host | tr -d '[:space:]' | read selected_host
   if [ $selected_host ]
-    if string match -r '(ECS|redash)' $name
+    if string match -ir 'ECS' $name
       commandline "ssh ec2-user@$selected_host -i ~/.ssh/keys/yamap.pem"
+    else if string match -ir 'redash' $name
+      commandline "ssh ubuntu@$selected_host -i ~/.ssh/keys/yamap.pem"
     else
       commandline "ssh $selected_host"
     end
