6076b917a52416d9500873b05f4436cad9e304ca
Hitoshi Nakashima <hitoshi.nakashima@gmail.com>
2018-07-03 14:09:03 +0900

    Use hub 2.4 for peco_github_prs.fish

--- .config/fish/functions/peco_github_prs.fish
+++ .config/fish/functions/peco_github_prs.fish
@@ -1,6 +1,8 @@
 function peco_github_prs
-  hub issue 2> /dev/null | grep 'pull' | peco --query "$LBUFFER" | sed -e 's/.*( \(.*\) )$/\1/' | read pr
-  if [ $pr ]
-    open $pr
+  hub pr list 2> /dev/null | peco --query "$LBUFFER" | sed -e 's/ *#\([^ ]..\).*/\1/' | read pr_id
+  if [ $pr_id ]
+    git remote show origin -n | grep 'Fetch URL:' | sed -e 's/  Fetch URL: //' | read fetch_url
+    echo "$fetch_url/pull/$pr_id" | read pr_url
+    open $pr_url
   end
 end
