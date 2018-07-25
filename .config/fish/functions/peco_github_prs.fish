6b5d6884367fda57f8c703e88135405c8d944c80
Hitoshi Nakashima <hitoshi.nakashima@gmail.com>
2018-07-25 16:52:45 +0900

    fish: Use perl to extract Pull Request ID

--- .config/fish/functions/peco_github_prs.fish
+++ .config/fish/functions/peco_github_prs.fish
@@ -1,5 +1,5 @@
 function peco_github_prs
-  hub pr list 2> /dev/null | peco --query "$LBUFFER" | sed -e 's/ *#\([^ ]..\).*/\1/' | read pr_id
+  hub pr list 2> /dev/null | peco --query "$LBUFFER" | perl -pe 's/\s+#(\d+?)[^0-9]+/$1/g' | read pr_id
   if [ $pr_id ]
     git remote show origin -n | grep 'Fetch URL:' | sed -e 's/  Fetch URL: //' | read fetch_url
     echo "$fetch_url/pull/$pr_id" | read pr_url
