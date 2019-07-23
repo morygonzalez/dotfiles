0fe2c7b6ffae2e365b9723dfb52c451c6c984918
Hitoshi Nakashima <hitoshi.nakashima@gmail.com>
2019-07-23 12:40:32 +0900

    fish: Consider feature env

--- .config/fish/functions/peco_ec2ls_development.fish
+++ .config/fish/functions/peco_ec2ls_development.fish
@@ -1,8 +1,8 @@
 function peco_ec2ls_development
-  ec2ls | grep 'dev' | peco --query "$argv" | awk -F\t '{print $4 $3}' | read selected_host name
+  ec2ls | grep 'dev\|feature' | peco --query "$argv" | awk -F\t '{print $4 $3}' | read selected_host name
   echo $selected_host | tr -d '[:space:]' | read selected_host
   if [ $selected_host ]
-    if string match -ir '(ECS|gyazo)' $name
+    if string match -ir '(ECS|gyazo|feature)' $name
       commandline "ssh ec2-user@$selected_host -i ~/.ssh/keys/yamap.pem"
     else
       commandline "ssh webapp@$selected_host"
