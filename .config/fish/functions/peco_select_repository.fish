2489a8956a401c6fdc45701e596851c9f67ba41f
Hitoshi Nakashima <morygonzalez@gmail.com>
2022-05-03 07:48:20 +0900

    fish: Upgrade to fish 3.4.1 and simplify peco repository select

--- .config/fish/functions/peco_select_repository.fish
+++ .config/fish/functions/peco_select_repository.fish
@@ -1,12 +1,8 @@
 function peco_select_repository
-  if set -q $argv
-    ghq list -p | peco | read line
-  else
-    ghq list -p | peco --query $argv | read line
+  set selected_repository (ghq list -p | peco --query "$LBUFFER")
+  if [ -n "$selected_repository" ]
+    cd $selected_repository
+    echo " $selected_repository "
+    commandline -f repaint
   end
-
-  if [ $line ]
-    builtin cd $line
-  end
-  commandline -f repaint
 end
