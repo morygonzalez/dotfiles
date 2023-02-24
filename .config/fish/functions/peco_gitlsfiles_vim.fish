b05c1e5047a555172252f557df84c9ee5bd515c8
Hitoshi Nakashima <morygonzalez@gmail.com>
2023-02-24 13:37:32 +0900

    fish: Consider mtime on peco_gitlsfiles

--- .config/fish/functions/peco_gitlsfiles_vim.fish
+++ .config/fish/functions/peco_gitlsfiles_vim.fish
@@ -1,5 +1,5 @@
 function peco_gitlsfiles_vim
-  git ls-files | peco --query "$LBUFFER" | read selected
+  git ls-files -z | xargs -0 ls -t | peco --query "$LBUFFER" | read selected
   if [ $selected ]
     vim $selected
   end
